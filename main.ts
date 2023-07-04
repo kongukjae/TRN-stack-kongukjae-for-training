be4f2701d1b0448299d173da7b3c7d1c757461a3
kongukjae <kongukjae@gmail.com>
2023-07-04 19:28:12 +0900

    bootstrap() 이라는 함수는 Nest.js에서 관용적으로 사용하는 함수 이름이며, css 라이브러리와는 다른 개념이다. Nest.js는 NestFactory모듈에서 create() 메서드로 웹 앱 서버를 지원한다.

--- main.ts
+++ main.ts
@@ -1,1 +1,11 @@
 // main.ts
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap(): Promise<void> {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
+}
+bootstrap().catch((err) => {
+  console.error(err);
+});
