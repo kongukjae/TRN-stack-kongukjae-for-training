262d44d86bae8e6c6a349fa3e80a83cf217a99a9
kongukjae <kongukjae@gmail.com>
2023-07-05 14:38:56 +0900

    @Get 데코레이터를 통한 get요청 처리, clien-side-rendering 이므로 페이지 조립과 서버 송출과는 다른 문제이다.

--- app.controller.ts
+++ app.controller.ts
@@ -1,9 +1,11 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
+import { Response } from 'express';
+import { join } from 'path';
 
 @Controller()
 export class AppController {
   @Get()
-  getHello(): string {
-    return 'This is Controller';
+  getIndex(@Res() res: Response): void {
+    res.sendFile(join(__dirname, 'public', 'index.html'));
   }
 }
