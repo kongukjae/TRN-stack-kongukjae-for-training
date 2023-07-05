3ee50eddfe9944662228a08affa48ceaf7bf9908
kongukjae <kongukjae@gmail.com>
2023-07-05 14:37:25 +0900

    단순 경로 조정

--- app.module.ts
+++ app.module.ts
@@ -7,7 +7,7 @@ import { ServeStaticModule } from '@nestjs/serve-static';
 @Module({
   imports: [
     ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', 'public'),
+      rootPath: join(__dirname, 'public'),
     }),
   ],
   controllers: [AppController],
