7fb74c9ba3d0225a886163851a126e49e4326964
kongukjae <kongukjae@gmail.com>
2023-07-05 14:18:27 +0900

    Nest.js module을 관리하는 파일 설정 작성

--- app.module.ts
+++ app.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { join } from 'path';
+import { ServeStaticModule } from '@nestjs/serve-static';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
